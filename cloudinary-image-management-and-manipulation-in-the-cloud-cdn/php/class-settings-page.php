b1376b066b60be8f11eb53bb93c012e63a366e35
Dukagjin Surdulli <me@dukagj.in>
2020-08-12 15:55:01 -0400

    Fix problem where radio labels don't show

--- cloudinary-image-management-and-manipulation-in-the-cloud-cdn/php/class-settings-page.php
+++ cloudinary-image-management-and-manipulation-in-the-cloud-cdn/php/class-settings-page.php
@@ -299,8 +299,21 @@ class Settings_Page implements Component\Assets, Component\Config, Component\Set
 			case 'radio':
 				foreach ( $field['choices'] as $key => $option ) :
 				?>
-					<input type="<?php echo esc_attr( $type ); ?>" class="cld-field regular-<?php echo esc_attr( $type ); ?>" id="<?php echo esc_attr( $field['label_for'] . '_' . $key ); ?>" name="<?php echo esc_attr( $setting_slug ); ?>[<?php echo esc_attr( $field['slug'] ); ?>]" <?php if ( ! empty( $field['pattern'] ) ) : ?>pattern="<?php echo esc_attr( $field['pattern'] ); ?>"<?php endif; ?> data-condition="<?php echo esc_attr( $condition ); ?>" data-context="<?php echo esc_attr( $context ); ?>" <?php echo esc_attr( $required ); ?> <?php checked( $key, $value ); ?> value="<?php echo esc_attr( $key ); ?>"> 
-					<label for="<?php echo esc_attr( $field['label_for'] . '_' . $key ); ?>"><?php esc_html( $option ); ?></label>
+					<input 
+						type="<?php echo esc_attr( $type ); ?>" 
+						class="cld-field regular-<?php echo esc_attr( $type ); ?>" 
+						id="<?php echo esc_attr( $field['label_for'] . '_' . $key ); ?>" 
+						name="<?php echo esc_attr( $setting_slug ); ?>[<?php echo esc_attr( $field['slug'] ); ?>]" 
+						<?php if ( ! empty( $field['pattern'] ) ) : ?>
+							pattern="<?php echo esc_attr( $field['pattern'] ); ?>"
+						<?php endif; ?> 
+						data-condition="<?php echo esc_attr( $condition ); ?>" 
+						data-context="<?php echo esc_attr( $context ); ?>" 
+						<?php echo esc_attr( $required ); ?> 
+						<?php checked( $key, $value ); ?> 
+						value="<?php echo esc_attr( $key ); ?>"
+					/> 
+					<label for="<?php echo esc_attr( $field['label_for'] . '_' . $key ); ?>"><?php esc_html_e( $option ); ?></label>
 				<?php
 				endforeach;
 				break;
