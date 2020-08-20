8d64777a7ea41e6f591d4661ba5c38c8159f5af9
David Cramer <david@digilab.co.za>
2020-08-20 11:04:15 +0200

    add notices methods and warning confirms in JS

--- cloudinary-image-management-and-manipulation-in-the-cloud-cdn/php/sync/class-storage.php
+++ cloudinary-image-management-and-manipulation-in-the-cloud-cdn/php/sync/class-storage.php
@@ -9,6 +9,7 @@ namespace Cloudinary;
 
 namespace Cloudinary\Sync;
 
+use Cloudinary\Component\Notice;
 use Cloudinary\Sync;
 
 /**
@@ -16,7 +17,7 @@ use Cloudinary\Sync;
  *
  * Handles filtering of HTML content.
  */
-class Storage {
+class Storage implements Notice {
 
 	/**
 	 * Holds the Plugin instance.
@@ -56,6 +57,22 @@ class Storage {
 	protected $download;
 
 	/**
+	 * Holds the Connect instance.
+	 *
+	 * @since   0.1
+	 *
+	 * @var     \Cloudinary\Connect Instance of the plugin.
+	 */
+	protected $connect;
+
+	/**
+	 * Holds an array of the storage settings.
+	 *
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
 	 * Filter constructor.
 	 *
 	 * @param \Cloudinary\Plugin $plugin The plugin.
@@ -65,6 +82,7 @@ class Storage {
 		$this->sync     = $this->plugin->components['sync'];
 		$this->media    = $this->plugin->components['media'];
 		$this->download = $this->sync->managers['download'];
+
 		add_action( 'cloudinary_register_sync_types', array( $this, 'setup' ), 20 );
 	}
 
@@ -74,9 +92,7 @@ class Storage {
 	 * @return string
 	 */
 	public function generate() {
-		$settings = $this->plugin->config['settings']['storage'];
-
-		return $settings['offload'] . $settings['low_res'];
+		return $this->settings['offload'] . $this->settings['low_res'];
 	}
 
 	/**
@@ -85,14 +101,13 @@ class Storage {
 	 * @param int $attachment_id The attachment ID.
 	 */
 	public function sync( $attachment_id ) {
-		$settings = $this->plugin->config['settings']['storage'];
 
-		switch ( $settings['offload'] ) {
+		switch ( $this->settings['offload'] ) {
 			case 'cld':
 				$this->remove_local_assets( $attachment_id );
 				break;
 			case 'dual_low':
-				$url = $this->media->cloudinary_url( $attachment_id, 'full', array( array( 'quality' => $settings['low_res'] ) ), null, false, true );
+				$url = $this->media->cloudinary_url( $attachment_id, 'full', array( array( 'effect' => 'blur:100', 'quality' => $this->settings['low_res'] . ':440' ) ), null, false, true );
 				break;
 			case 'dual_full':
 				$state = $this->media->get_post_meta( $attachment_id, Sync::META_KEYS['storage'], true );
@@ -110,9 +125,8 @@ class Storage {
 		}
 
 		$this->sync->set_signature_item( $attachment_id, 'storage' );
-		$this->sync->set_signature_item( $attachment_id, 'file' );
-		$this->sync->set_signature_item( $attachment_id, 'suffix' );
-		$this->media->update_post_meta( $attachment_id, Sync::META_KEYS['storage'], $settings['offload'] ); // Save the state.
+		$this->sync->set_signature_item( $attachment_id, 'breakpoints' );
+		$this->media->update_post_meta( $attachment_id, Sync::META_KEYS['storage'], $this->settings['offload'] ); // Save the state.
 	}
 
 	/**
@@ -133,9 +147,8 @@ class Storage {
 	 * @return string
 	 */
 	public function status() {
-		$settings = $this->plugin->config['settings']['storage'];
-		$note     = __( 'Syncing', 'cloudinary' );
-		switch ( $settings['offload'] ) {
+		$note = __( 'Syncing', 'cloudinary' );
+		switch ( $this->settings['offload'] ) {
 			case 'cld':
 				$note = __( 'Removing local copies.', 'cloudinary' );
 				break;
@@ -151,12 +164,69 @@ class Storage {
 	}
 
 	/**
+	 * Get notices to display in admin.
+	 *
+	 * @return array|void
+	 */
+	public function get_notices() {
+		$notices = array();
+		if ( 'cld' === $this->settings['offload'] ) {
+			$storage         = $this->connect->get_usage_stat( 'storage', 'used_percent' );
+			$transformations = $this->connect->get_usage_stat( 'transformations', 'used_percent' );
+			$bandwidth       = $this->connect->get_usage_stat( 'bandwidth', 'used_percent' );
+			if ( 100 <= $storage || 100 <= $transformations || 100 <= $bandwidth ) {
+
+				$html = array(
+					'<div>' . __( 'You have reached one or more of your quota limits. Soon media may not be delivered. Since all assets are on Cloudinary, this will result in broken images.' ) . '</div>',
+					'<div>' . __( 'To rectify this you have some options:' ) . '</div>',
+					'<ol>',
+					'<li>' . sprintf(
+						__(
+							'<a href="%1$s" target="_blank">%2$s</a>',
+							'cloudinary'
+						),
+						'https://cloudinary.com/console/lui/upgrade_options',
+						__( 'Upgrade your account', 'cloudinary' )
+					) . '</li>',
+					'<li>' . __( 'Set storage to Cloudinary low resolution on WordPress.' ) . '</li>',
+					'<li>' . __( 'Cloudinary and WordPress' ) . '</li>',
+					'</ol>',
+				);
+
+				$notices[] = array(
+					'message'     => implode( '', $html ),
+					'type'        => 'error',
+					'dismissible' => true,
+				);
+			}
+		}
+
+		return $notices;
+	}
+
+	/**
+	 * Add a deactivate class to the deactivate link to trigger a warning if storage is only on Cloudinary.
+	 *
+	 * @param array $actions The actions for the plugin.
+	 *
+	 * @return array
+	 */
+	public function tag_deactivate_link( $actions ) {
+		if ( 'cld' === $this->settings['offload'] ) {
+			$actions['deactivate'] = str_replace( '<a ', '<a class="cld-deactivate" ', $actions['deactivate'] );
+		}
+
+		return $actions;
+	}
+
+	/**
 	 * Setup hooks for the filters.
 	 */
 	public function setup() {
-		$this->sync = $this->plugin->components['sync'];
-
-		$structure = array(
+		$this->sync     = $this->plugin->components['sync'];
+		$this->connect  = $this->sync->managers['connect'];
+		$this->settings = $this->plugin->config['settings']['storage'];
+		$structure      = array(
 			'generate' => array( $this, 'generate' ),
 			'priority' => 5.2,
 			'sync'     => array( $this, 'sync' ),
@@ -164,5 +234,9 @@ class Storage {
 			'note'     => array( $this, 'status' ),
 		);
 		$this->sync->register_sync_type( 'storage', $structure );
+
+		// Tag the deactivate button.
+		$plugin_file = pathinfo( dirname( CLDN_CORE ), PATHINFO_BASENAME ) . '/' . basename( CLDN_CORE );
+		add_filter( 'plugin_action_links_' . $plugin_file, array( $this, 'tag_deactivate_link' ) );
 	}
 }
