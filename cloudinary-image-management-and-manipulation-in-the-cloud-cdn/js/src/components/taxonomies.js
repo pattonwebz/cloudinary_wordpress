675024395e4a331d4a2ec1ac5fcbf2ac30fb0480
Dugi Surdulli <dugisurdulli@gmail.com>
2020-09-16 12:29:40 -0400

    Ensure that the toggle is present before attempting to attach event

--- cloudinary-image-management-and-manipulation-in-the-cloud-cdn/js/src/components/taxonomies.js
+++ cloudinary-image-management-and-manipulation-in-the-cloud-cdn/js/src/components/taxonomies.js
@@ -1,13 +1,17 @@
-document.querySelector( '.cloudinary-collapsible__toggle' ).addEventListener( 'click', function () {
-	const content = document.querySelector( '.cloudinary-collapsible__content' );
-	const isHidden = window.getComputedStyle( content, null ).getPropertyValue( 'display' ) === 'none';
-	const arrowIcon = document.querySelector( '.cloudinary-collapsible__toggle button i' );
+const toggler = document.querySelector( '.cloudinary-collapsible__toggle' );
 
-	content.style.display = isHidden ? 'block' : 'none'
-
-	if ( arrowIcon.classList.contains( 'dashicons-arrow-down-alt2' ) ) {
-		arrowIcon.classList.replace( 'dashicons-arrow-down-alt2', 'dashicons-arrow-up-alt2' );
-	} else {
-		arrowIcon.classList.replace( 'dashicons-arrow-up-alt2', 'dashicons-arrow-down-alt2' );
-	}
-} );
+if ( toggler ) {
+  toggler.addEventListener( 'click', function () {
+    const content = document.querySelector( '.cloudinary-collapsible__content' );
+    const isHidden = window.getComputedStyle( content, null ).getPropertyValue( 'display' ) === 'none';
+    const arrowIcon = document.querySelector( '.cloudinary-collapsible__toggle button i' );
+  
+    content.style.display = isHidden ? 'block' : 'none'
+  
+    if ( arrowIcon.classList.contains( 'dashicons-arrow-down-alt2' ) ) {
+      arrowIcon.classList.replace( 'dashicons-arrow-down-alt2', 'dashicons-arrow-up-alt2' );
+    } else {
+      arrowIcon.classList.replace( 'dashicons-arrow-up-alt2', 'dashicons-arrow-down-alt2' );
+    }
+  } );
+}
