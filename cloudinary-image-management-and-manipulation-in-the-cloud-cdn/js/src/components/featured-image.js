feeb6afa6b3a810a78c53433b3b40fe020080967
David Cramer <david@digilab.co.za>
2020-09-21 08:46:14 +0200

    check for modal class

--- cloudinary-image-management-and-manipulation-in-the-cloud-cdn/js/src/components/featured-image.js
+++ cloudinary-image-management-and-manipulation-in-the-cloud-cdn/js/src/components/featured-image.js
@@ -10,11 +10,13 @@ let FeaturedTransformationsToggle = ( props ) => {
 
     return (
         <>
+            { props.modalClass &&
             <ToggleControl
                 label={ __( 'Overwrite Transformations', 'cloudinary' ) }
                 checked={ props.overwrite_featured_transformations }
                 onChange={ ( value ) => props.setOverwrite( value ) }
             />
+            }
         </>
     );
 };
