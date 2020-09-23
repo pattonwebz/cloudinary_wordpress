b47a2486d70be29aa071d2bfd3b894ac4f29d6d5
David Cramer <david@digilab.co.za>
2020-09-23 13:35:07 +0200

    remove clean params

--- cloudinary-image-management-and-manipulation-in-the-cloud-cdn/php/sync/class-storage.php
+++ cloudinary-image-management-and-manipulation-in-the-cloud-cdn/php/sync/class-storage.php
@@ -181,13 +181,13 @@ class Storage implements Notice {
 					// Add low quality transformations.
 					$transformations[] = array( 'quality' => 'auto:low' );
 				}
-				$url = $this->media->cloudinary_url( $attachment_id, '', $transformations, null, false, true );
+				$url = $this->media->cloudinary_url( $attachment_id, '', $transformations, null, false );
 				break;
 			case 'dual_full':
 				if ( ! empty( $previous_state ) && 'dual_full' !== $previous_state ) {
 					// Only do this is it's changing a state.
 					$transformations = $this->media->get_transformation_from_meta( $attachment_id );
-					$url             = $this->media->cloudinary_url( $attachment_id, '', $transformations, null, false, false );
+					$url             = $this->media->cloudinary_url( $attachment_id, '', $transformations, null, false );
 				}
 				break;
 		}
