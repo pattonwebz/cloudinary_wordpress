cb04ec6f3892f9a49af86e0fcdb7eff7b78fe697
Marco Pereirinha <marco.pereirinha@xwp.co>
2020-10-12 15:14:54 +0100

    Add comments

--- js/src/deactivate.js
+++ js/src/deactivate.js
@@ -1,17 +1,27 @@
 /* global window wp cloudinaryApi */
 
 const Deactivate = {
+	// The link that triggers the ThickBox
 	tbLink: document.getElementById( 'cld-deactivation-link' ),
+	// The different links to deactivate the plugin.
 	pluginListLinks: document.querySelectorAll( '.cld-deactivate-link, .cld-deactivate' ),
+	// The reasons.
 	options: document.querySelectorAll( '.cloudinary-deactivation input[type="radio"]' ),
+	// The feedback submit button.
 	submitButton: document.querySelector( '.cloudinary-deactivation .button-primary' ),
+	// The skip button.
 	skipButton: document.querySelector( '.cloudinary-deactivation .button-link' ),
+	// Selected reason.
 	reason: '',
+	// The more details .
 	more: null,
+	// The deactivation link for the plugin.
 	deactivationUrl: '',
 
 	addEvents: function() {
 		const context = this;
+
+		// Add event listener to deativation links to add the pop up.
 		[ ...context.pluginListLinks ].forEach( ( link ) => {
 			link.addEventListener( 'click', function( ev ) {
 				ev.preventDefault();
@@ -31,10 +41,12 @@ const Deactivate = {
 			});
 		});
 
+		// Add event listener to skip feedback.
 		context.skipButton.addEventListener( 'click', function( ev ) {
 			window.location.href = context.deactivationUrl;
 		});
 
+		// Add event listener to update reason and more container.
 		[ ...context.options ].forEach( (option) => {
 			option.addEventListener( 'change', function( ev ) {
 				context.submitButton.removeAttribute( 'disabled' );
@@ -43,6 +55,7 @@ const Deactivate = {
 			});
 		});
 
+		// Add event listener to submit the feedbacl.
 		context.submitButton.addEventListener( 'click', function( ev ) {
 			wp.ajax.send( {
 				url: cloudinaryApi.endpoint,
@@ -60,7 +73,7 @@ const Deactivate = {
 	},
 
 	/**
-	 *
+	 * Init method.
 	 */
 	init: function() {
 		this.addEvents();
