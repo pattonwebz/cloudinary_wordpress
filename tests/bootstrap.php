8dd8894ab51248d4ecfafa6682882d43a7d8d52f
David Cramer <david@digilab.co.za>
2020-11-30 14:30:40 +0200

    update bootstraps

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,7 @@
  * @package FooBar
  */
 
-global $_plugin_files;
-$_plugin_files = array();
+global $_plugin_root;
 
 $_plugin_root = realpath( __DIR__ . '/..' );
 
@@ -28,43 +27,14 @@ if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
 
 require_once $_tests_dir . '/includes/functions.php';
 
-// Build the plugins directory search array.
-$_plugins_array = glob( realpath( __DIR__ . '/../..' ) . '/*' );
-
-// Build the plugin files array.
-foreach ( $_plugins_array as $_plugin_candidate ) {
-	if ( is_dir( $_plugin_candidate ) && 'akismet' !== basename( $_plugin_candidate ) ) {
-		foreach ( glob( $_plugin_candidate . '/*.php' ) as $_plugin_file_candidate ) {
-			if ( basename( $_plugin_file_candidate ) !== 'cloudinary.php' && basename( $_plugin_candidate ) !== basename( $_plugin_file_candidate, '.php' ) ) {
-				continue;
-			}
-			// @codingStandardsIgnoreStart
-			$_plugin_file_src = file_get_contents( $_plugin_file_candidate );
-			// @codingStandardsIgnoreEnd
-			if ( preg_match( '/Plugin\s*Name\s*:/', $_plugin_file_src ) ) {
-				$_plugin_files[] = $_plugin_file_candidate;
-				break;
-			}
-		}
-	}
-}
-
-if ( empty( $_plugin_files ) ) {
-	trigger_error( 'Unable to locate any files containing a plugin metadata block.', E_USER_ERROR ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
-}
-
-unset( $_plugins, $_plugin_candidate, $_plugin_file_candidate, $_plugin_file_src );
-
 /**
  * Loads the plugins for testing.
  */
 function unit_test_load_plugin_file() {
-	global $_plugin_files;
+	global $_plugin_root;
 	// Load the plugins.
-	foreach ( $_plugin_files as $file ) {
-		require_once "$file";
-	}
-	unset( $_plugin_files );
+	require_once $_plugin_root . '/cloudinary.php';
+	unset( $_plugin_root );
 }
 
 tests_add_filter( 'muplugins_loaded', 'unit_test_load_plugin_file' );
---
+++ tests/phpunit/php/class-test-plugin.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Tests for Plugin class.
+ *
+ * @package Cloudinary
+ */
+
+namespace Cloudinary;
+
+/**
+ * Tests for Plugin class.
+ *
+ * @group   plugin
+ *
+ * @package Cloudinary
+ */
+class Test_Plugin extends \WP_UnitTestCase {
+
+	/**
+	 * Plugin instance.
+	 *
+	 * @var Plugin
+	 */
+	public $plugin;
+
+	/**
+	 * Setup.
+	 *
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->plugin = get_plugin_instance();
+	}
+
+	/**
+	 * Test constructor.
+	 *
+	 * @see Plugin::__construct()
+	 */
+	public function test_construct() {
+		$this->assertEquals( 9, has_action( 'plugins_loaded', array( $this->plugin, 'init' ) ) );
+		$this->assertEquals( 11, has_action( 'admin_enqueue_scripts', array( $this->plugin, 'enqueue_assets' ) ) );
+		$this->assertEquals( 10, has_action( 'init', array( $this->plugin, 'setup' ) ) );
+		$this->assertEquals( 10, has_action( 'init', array( $this->plugin, 'register_assets' ) ) );
+	}
+
+	/**
+	 * Test for init() method.
+	 *
+	 * @see Plugin::init()
+	 */
+	public function test_init() {
+		$plugin = get_plugin_instance();
+		$plugin->init();
+
+		$this->assertInternalType( 'array', $plugin->config );
+		$this->assertArrayHasKey( 'settings', $plugin->config );
+	}
+
+	/**
+	 * Test locate_plugin.
+	 *
+	 * @see Plugin::locate_plugin()
+	 */
+	public function test_locate_plugin() {
+		$location   = $this->plugin->locate_plugin();
+		$actual_dir = basename( dirname( dirname( __DIR__ ) ) );
+		$this->assertEquals( $actual_dir, $location['dir_basename'] );
+		$this->assertContains( 'plugins/cloudinary', $location['dir_path'] );
+		$this->assertContains( 'plugins/cloudinary', $location['dir_url'] );
+	}
+
+	/**
+	 * Test relative_path.
+	 *
+	 * @see Plugin::relative_path()
+	 */
+	public function test_relative_path() {
+		$this->assertEquals( 'plugins/cloudinary', $this->plugin->relative_path( '/srv/www/wordpress-develop/src/wp-content/plugins/cloudinary', 'wp-content', '/' ) );
+		$this->assertEquals( 'themes/twentysixteen/plugins/cloudinary', $this->plugin->relative_path( '/srv/www/wordpress-develop/src/wp-content/themes/twentysixteen/plugins/cloudinary', 'wp-content', '/' ) );
+	}
+}
