dd60e5067a897b1fc5e3a98f76eb7bb6912f4228
Marco Pereirinha <marco.pereirinha@xwp.co>
2020-12-10 16:44:42 +0000

    Update plan contents

--- php/ui/component/class-plan.php
+++ php/ui/component/class-plan.php
@@ -9,6 +9,8 @@ namespace Cloudinary\UI\Component;
 
 use function Cloudinary\get_plugin_instance;
 use Cloudinary\UI\Component;
+use Cloudinary\Settings\Setting;
+use Cloudinary\Connect;
 
 /**
  * Plan Component to render plan status.
@@ -25,11 +27,28 @@ class Plan extends Component {
 	protected $blueprint = 'title/|plan_box|plan_heading/|plan_summary/|/plan_box';
 
 	/**
+	 * Holder the Connect object.
+	 *
+	 * @var Connect
+	 */
+	protected $connection;
+
+	/**
+	 * Plan constructor.
+	 *
+	 * @param Setting $setting The parent Setting.
+	 */
+	public function __construct( $setting ) {
+		$this->connection = get_plugin_instance()->get_component( 'connect' );
+
+		parent::__construct( $setting );
+	}
+
+	/**
 	 * Setup action before rendering.
 	 */
 	protected function pre_render() {
-		$connection = get_plugin_instance()->get_component( 'connect' );
-		$this->setting->set_param( 'plan_heading', $connection->get_usage_stat( 'plan' ) );
+		$this->setting->set_param( 'plan_heading', $this->connection->get_usage_stat( 'plan' ) );
 	}
 
 	/**
@@ -70,7 +89,7 @@ class Plan extends Component {
 	 * @return array
 	 */
 	protected function plan_heading( array $struct ) {
-		$struct['element'] = 'h3';
+		$struct['element']             = 'h3';
 		$struct['attributes']['class'] = array(
 			'cld-plan',
 		);
@@ -88,14 +107,14 @@ class Plan extends Component {
 	protected function plan_summary( $struct ) {
 
 		$summary            = $this->get_part( 'h4' );
-		$summary['content'] = $this->setting->get_param( 'plan_heading', __( '25 Monthly Credits', 'cloudinary' ) );
+		$summary['content'] = $this->get_plan_description();
 
 		$detail            = $this->get_part( 'span' );
 		$detail['content'] = __( '1 Credit =', 'cloudinary' );
 
 		$struct['children']['title'] = $summary;
 		$struct['children']['span']  = $detail;
-		$struct['children']['ul']    = $this->content();
+		$struct['children']['ul']    = $this->credits_content();
 		$struct['element']           = 'div';
 
 		return $struct;
@@ -118,11 +137,28 @@ class Plan extends Component {
 	}
 
 	/**
+	 * Get plan description.
+	 *
+	 * @return string
+	 */
+	protected function get_plan_description() {
+		$description = __( 'Pay as you go', 'cloudinary' );
+		$limit       = $this->connection->get_usage_stat( 'limit' );
+
+		if ( $limit ) {
+			// translators: The number of monthly credits.
+			$description = _n( '%d Monthly Credit', '%d Monthly Credits', $limit, 'cloudinary' );
+		}
+
+		return $description;
+	}
+
+	/**
 	 * Creates the bullet points of the plan.
 	 *
 	 * @return array
 	 */
-	protected function content() {
+	protected function credits_content() {
 
 		$points = $this->get_part( 'ul' );
 		$items  = array(
